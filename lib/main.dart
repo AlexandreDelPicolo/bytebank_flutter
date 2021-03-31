387fba70de2122dd543b7efc0e31cdea337b3e9b
Alexandre Picolo <alexandre.picolo@midway.com.br>
2021-03-30 22:01:38 -0300

    Criando estrutura inicial da tela

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,25 @@ void main() => runApp(MaterialApp(
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('22451-8'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.0'),
+                subtitle: Text('22451-8'),
+              ),
+            ),
+          ],
         ),
       ),
     ));
