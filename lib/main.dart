127e47943edce40e087670d9f81c30b7bb8c03d1
Alexandre Picolo <alexandre.picolo@midway.com.br>
2021-04-12 20:39:12 -0300

    Customização de cores

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-      ),
+      home: ListaTransferencias(),
+      // theme: ThemeData.dark(),
+      // Material tool color
+      // https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=4FC3F7
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary)),
     );
   }
 }
